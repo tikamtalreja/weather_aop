46c07bbd07d3c9fc69bd4713a07376e8810d7979
Tikam Talreja <tikamtalreja@gmail.com>
2019-05-22 23:21:12 +0530

    Update home.dart

--- lib/UI/home.dart
+++ lib/UI/home.dart
@@ -6,96 +6,103 @@ import 'package:http/http.dart' as http;
 
 
 
-class MyHome extends StatefulWidget {
+class WeatherHome extends StatefulWidget {
   @override
-  _MyHomeState createState() => _MyHomeState();
+  _WeatherHomeState createState() => _WeatherHomeState();
 }
 
-class _MyHomeState extends State<MyHome> {
-  var _textController = new TextEditingController();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar:  AppBar(
-        title: new Text("Weather App"),
-      ),
-      body: new Center(
-        child: Column(
-          children: <Widget>[
-            new ListTile(
-            title: new TextField(
-              decoration: InputDecoration(
-                hintText: "city name",
-                border: OutlineInputBorder()
-              ),
-              controller: _textController,
-            ),
-          ),
-            RaisedButton(child: Text("see the weather report"),
-            onPressed: (){
-              var route = new MaterialPageRoute(
-                  builder: (BuildContext context) =>
-                      new MyHomePage(value: _textController.text),
-                );
-                Navigator.of(context).push(route);
-            },)
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class MyHomePage extends StatefulWidget {
-  final String value;
-
-  MyHomePage({Key key, this.value}) : super(key: key);
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-    
+class _WeatherHomeState extends State<WeatherHome> {
+  TextEditingController textFieldController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-       
-        centerTitle: true,
-  title: Text('Weather App'),
-),
-     body: new Stack(
+        title: Text("Weather Home page"),
+        
+      ),
+     
+      body: Stack(
+        children: <Widget>[
+          Image.network("https://images.unsplash.com/photo-1489549132488-d00b7eee80f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          fit: BoxFit.fill,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: textFieldController,
+              autofocus: true,
+              keyboardType: TextInputType.text,
+                 style: TextStyle(fontSize: 20,color: Colors.white),
+        decoration: InputDecoration(
+          labelText: "City Name",
+          hintStyle: TextStyle(fontSize: 20.0, color: Colors.white),
+          border: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.teal,
+            ),
+          ),
+          prefixIcon: const Icon(
+            Icons.location_city,
+            color: Colors.white,
+          ),
+        ),
+              ),
+          ),
+          Positioned(
+            bottom: 20.0,
+            left: 100.0,
+            child: Center(
+              child: RaisedButton(
+                splashColor: Colors.indigoAccent,
+                shape: Border.all(color: Colors.white),
+                color: Colors.indigo,
+                
+                
+                onPressed: (){
+                   _sendDataToSecondScreen(context);
+                                 },
+                                 child: Text("get weather report",
+                                 style: TextStyle(color: Colors.white,),
+                                 ),
+                               ),
+            ),
+                           )
+                         ],
+                       ),
+                     );
+                   }
+                 
+                   void _sendDataToSecondScreen(BuildContext context) {
+                     String textToSend = textFieldController.text;
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => WeatherReport(text: textToSend,),
+        ));
+                   }
+}
+
+class WeatherReport extends StatefulWidget {
+   final String text;
+   WeatherReport({Key key, @required this.text}) : super(key: key);
+  @override
+  _WeatherReportState createState() => _WeatherReportState();
+}
+
+class _WeatherReportState extends State<WeatherReport> {
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("report"),
+      ),
+      body: new Stack(
        children: <Widget>[
          new Center(
-           child: new Image.asset("images/p1.jpg",
+           child: new Image.network("https://images.unsplash.com/photo-1508898578281-774ac4893c0c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
            height: MediaQuery.of(context).size.height,
            width: MediaQuery.of(context).size.width,
            fit: BoxFit.fill,
@@ -106,15 +113,15 @@ class _MyHomePageState extends State<MyHomePage> {
          new Container(
            alignment: Alignment.topRight,
            margin: const EdgeInsets.fromLTRB(0.0, 15.9, 20.9, 0.0),
-           child: new Text("${widget.value}",
+           child: new Text("${widget.text}",
            style: cityStyle(),
          )
          ),
 // weather data
   new Container(
     alignment: Alignment.bottomLeft,
-    margin: const EdgeInsets.fromLTRB(12.0, 0.0, 0.0, 130),
-    child: updateTempWidget("${widget.value}")
+    margin: const EdgeInsets.fromLTRB(20.0, 40.0, 0.0, 130),
+    child: updateTempWidget("${widget.text}")
     
   )
 
@@ -127,7 +134,7 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
  Future<Map> getWeather(String appid,String city) async{
-   String apiurl= "http://api.openweathermap.org/data/2.5/weather?q=$city&appid=${util.apiId}&units=imperial";
+   String apiurl= "http://api.openweathermap.org/data/2.5/weather?q=$city&appid=${util.apiId}&units=metric";
    http.Response response = await http.get(apiurl);
     return json.decode(response.body);
  }
@@ -140,31 +147,43 @@ class _MyHomePageState extends State<MyHomePage> {
           Map content = snapshot.data;
           return Center(
             child: Padding(
-              padding: const EdgeInsets.fromLTRB(8.0,200.0,0.0,0.0),
+              padding: const EdgeInsets.fromLTRB(8.0,300.0,0.0,0.0),
               child: new Container(
                 child: new Column(
                   
                   children: <Widget>[
-                    new ListTile(
-                      title: new Text(content['main']['temp'].toString(),
-                      style: tempStyle(),
-                      ),
-                    ), new ListTile(
-                      title: new Text(content['main']['temp_min'].toString(),
-                      style: tempStyle(),
-                      ),
+                    Row(
+                      children: <Widget>[
+                        Text("Current Temp :",
+                      style: tempStyle()),
+                        Text(content['main']['temp'].toString(),
+                      style: tempStyle())
+                      ],
                     ),
-                    new ListTile(
-                      title: new Text(content['main']['temp_max'].toString(),
-                      style: tempStyle(),
-                      ),
+                    Row(
+                      children: <Widget>[
+                        Text("Minimum Temp :",
+                      style: tempStyle()),
+                        Text(content['main']['temp_min'].toString(),
+                      style: tempStyle())
+                      ],
                     ),
-                     new ListTile(
-                      title: new Text(content['wind']['speed'].toString(),
-                      style: tempStyle(),
-                      ),
+                    Row(
+                      children: <Widget>[
+                        Text("Maximum Temp :",
+                      style: tempStyle()),
+                        Text(content['main']['temp_max'].toString(),
+                      style: tempStyle())
+                      ],
                     ),
-                    
+                    Row(
+                      children: <Widget>[
+                        Text("Wind Speed :",
+                      style: tempStyle()),
+                        Text(content['wind']['speed'].toString(),
+                      style: tempStyle())
+                      ],
+                    ),
                   ],
                 ),
               ),
@@ -178,34 +197,6 @@ class _MyHomePageState extends State<MyHomePage> {
  }
 
 }
-
-
-class ChangeCity extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return  new Scaffold(appBar: AppBar(
-      backgroundColor:Colors.transparent,
-      title: Text("change  city"),
-      centerTitle: true,
-    ),
-      body: new Stack(
-        children: <Widget>[
-          new ListView(
-            children: <Widget>[
-              new Image.asset("images/p1/jpg",
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              fit: BoxFit.fill,
-              ),
-              
-            ],
-          )
-        ],
-      ),
-    );
-  }
-}
-
 TextStyle cityStyle(){
   return new TextStyle(
     color: Colors.white,
@@ -217,21 +208,11 @@ TextStyle cityStyle(){
 TextStyle tempStyle(){
   return new TextStyle(
     color: Colors.white,
-    fontSize: 30.9,
+    fontSize: 20.9,
     fontStyle: FontStyle.normal,
-    fontWeight: FontWeight.w600,
+    fontWeight: FontWeight.w400,
     
   );
 }
-
-
-
-
-
-
-
- 
-
-
 
 
